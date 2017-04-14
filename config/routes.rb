a01bedf380bfa0ba2430ed383ff7d26ec8756b2d
chenzhiduo <zhiduochen@gmail.com>
2017-04-14 17:21:51 +0800

    step7 upload attachment

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
         end
       end
     end
+
+    resources :jobs do
+      resources :resumes
+    end
+
     root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
